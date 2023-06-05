113b44eff0a10c279e136ad153f11726b73db52e
Mo3110 <niche.hunter.31@gmail.com>
2023-06-05 15:55:20 +0300

    Edit

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
-import { exampleRouter } from "y/server/api/routers/example";
 import { createTRPCRouter } from "y/server/api/trpc";
+import { tweetRouter } from "y/server/api/routers/tweet";
+import { profileRouter } from "./routers/profile";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +8,8 @@ import { createTRPCRouter } from "y/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  tweet: tweetRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
